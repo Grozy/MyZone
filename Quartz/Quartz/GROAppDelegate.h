e6eec772919bb7e910000e7156a47d50c5d1cc34
Grozy <grozysun@gmail.com>
2014-06-06 10:49:02 +0800

    QuartzTest

---
+++ Quartz/Quartz/GROAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GROAppDelegate.h
+//  Quartz
+//
+//  Created by SunGuozhi on 14-6-6.
+//  Copyright (c) 2014年 SunGuozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GROAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quartz/Quartz/GROViewController.h
@@ -0,0 +1,13 @@
+//
+//  GROViewController.h
+//  Quartz
+//
+//  Created by SunGuozhi on 14-6-6.
+//  Copyright (c) 2014年 SunGuozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GROViewController : UIViewController
+
+@end
