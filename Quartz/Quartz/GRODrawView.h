b598503bbcf63e3b49cdbf2aff560bf33da05e13
Grozy <grozysun@gmail.com>
2014-06-06 10:51:31 +0800

    DrawView

---
+++ Quartz/Quartz/GRODrawView.h
@@ -0,0 +1,13 @@
+//
+//  GRODrawView.h
+//  Quartz
+//
+//  Created by SunGuozhi on 14-6-6.
+//  Copyright (c) 2014年 SunGuozhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GRODrawView : UIView
+
+@end
---
+++ Quartz/Quartz/GRODrawView.m
@@ -0,0 +1,13 @@
+//
+//  GRODrawView.m
+//  Quartz
+//
+//  Created by SunGuozhi on 14-6-6.
+//  Copyright (c) 2014年 SunGuozhi. All rights reserved.
+//
+
+#import "GRODrawView.h"
+
+@implementation GRODrawView
+
+@end
